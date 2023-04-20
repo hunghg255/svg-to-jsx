c2c35a2052c25e84f480014f48a0e3431aabe58b
hunghg255 <giahunghust@gmail.com>
2023-04-20 10:22:09 +0700

    update

---
+++ README.md
@@ -0,0 +1,18 @@
+# Script generate svg react component
+
+
+[![npm version](https://badge.fury.io/js/agile-svg2jsx.svg)](https://badge.fury.io/js/agile-svg2jsx) [![npm](https://img.shields.io/npm/dt/agile-svg2jsx.svg?logo=npm)](https://www.npmjs.com/package/agile-svg2jsx) [![npm](https://img.shields.io/bundlephobia/minzip/agile-svg2jsx)](https://www.npmjs.com/package/agile-svg2jsx)
+[![All Contributors](https://img.shields.io/badge/all_contributors-1-orange.svg)](#contributors-)
+
+## Add script package.json
+
+```bash
+ "scripts": {
+    ...
+    "g": "npx agile-svg2jsx --i $PATH_SVGS_FOLDER --o $PATH_REACT_FOLDER"
+  },
+```
+
+## Demo
+
+![Demo](./public/demo.gif)
---
+++ src/index.js
@@ -0,0 +1,154 @@
+#! /usr/bin/env node
+
+import * as path from 'path';
+import * as fs from 'fs';
+import { promisify } from 'util';
+import * as globby from 'globby';
+import HTMLtoJSX from 'htmltojsx';
+import { parseXml } from '@rgrove/parse-xml';
+import { stringify } from 'svgson';
+import * as commander from 'commander';
+
+commander.program.option("--i, --input <input>", "Input");
+commander.program.option("--o, --output <ouput>", "Ouput");
+commander.program.parse();
+
+const options = commander.program.opts();
+
+const PATH_FOLDER_SVGS = options.input;
+const PATH_FOLDER_ICONS = options.output;
+
+const converter = new HTMLtoJSX({
+  createClass: false,
+});
+
+const writeFile = promisify(fs.writeFile);
+const __dirname = path.resolve();
+
+/**
+ * generateObjSvg
+ * @returns [{ name: 'IconAbc', svg: '<svg>...</svg>'}]
+ */
+const generateObjSvg = () => {
+  const listSvgObj = [];
+
+  const formatChildrenSvg = (children) => {
+    const newChildren = [];
+    const childrenPath = children.filter((it) => it.name === 'path');
+
+    for (let idx = 0; idx < childrenPath.length; idx++) {
+      const element = childrenPath[idx];
+
+      if (element.name === 'path') {
+        if (childrenPath.length === 1) delete element.attributes.fill;
+        element.children = formatChildrenSvg(element.children);
+        newChildren.push(element);
+      }
+    }
+
+    return newChildren;
+  };
+
+  const formatSvgAst = (obj) => {
+    if (obj.name === 'svg') {
+      obj.attributes.fill = 'currentColor';
+      obj.attributes.width = '1em';
+      obj.attributes.height = '1em';
+    }
+
+    obj.children = formatChildrenSvg(obj.children);
+
+    return obj;
+  };
+
+  const capitalizeFirstLetter = (string) => string[0].toUpperCase() + string.slice(1);
+
+  const slugify = (str) =>
+    str
+      .toLowerCase()
+      .trim()
+      .replace(/[^\w\s-]/g, '')
+      .replace(/[\s_-]+/g, '-')
+      .replace(/^-+|-+$/g, '');
+
+  const updateCase = (name) => name.split('-').map(capitalizeFirstLetter).join('');
+
+  // Gen Name
+  globby.globbySync(`${PATH_FOLDER_SVGS}/*.svg`, { cwd: __dirname }).forEach((p) => {
+    const parsed = path.parse(p);
+    const { name } = parsed;
+    listSvgObj.push({ name: 'Icon' + updateCase(slugify(name)), path: p });
+  });
+
+  // Gen Svg
+  for (let idx = 0; idx < listSvgObj.length; idx++) {
+    const svgPath = listSvgObj[idx];
+
+    const data = fs.readFileSync(svgPath.path, 'utf8');
+    listSvgObj[idx].svg = stringify(
+      formatSvgAst(JSON.parse(JSON.stringify(parseXml(data).children[0]))),
+    );
+  }
+
+  return listSvgObj;
+};
+
+const generateIcons = async () => {
+  if (!PATH_FOLDER_SVGS || !PATH_FOLDER_ICONS) {
+    console.log(`\n ⚠️  Generate failed: Missed input or ouput!\n`);
+
+    return;
+  }
+
+  const iconsDir = path.join(__dirname, PATH_FOLDER_ICONS);
+
+  try {
+    await promisify(fs.access)(iconsDir);
+  } catch (err) {
+    await promisify(fs.mkdir)(iconsDir);
+  }
+
+  const listSvgObj = generateObjSvg();
+
+  if (!listSvgObj.length) {
+    console.log(`\n ⚠️  Can not found svg!\n`);
+
+    return;
+  }
+
+  const render = ({ svgIdentifier, content }) => {
+    return `
+import * as React from 'react';
+
+const ${svgIdentifier} = (props: React.HTMLAttributes<HTMLSpanElement>) => {
+  return <span {...props}>
+        ${content}
+  </span>;
+};
+
+${svgIdentifier}.displayName = '${svgIdentifier}';
+
+export default ${svgIdentifier};
+`;
+  };
+
+  for (let idx = 0; idx < listSvgObj.length; idx++) {
+    const element = listSvgObj[idx];
+
+    writeFile(
+      path.resolve(__dirname, `${PATH_FOLDER_ICONS}/${element.name}.tsx`),
+      render({ svgIdentifier: element.name, content: converter.convert(element.svg).trim() }),
+    );
+  }
+
+  // generate icon index
+  const entryText = listSvgObj
+    .map((element) => `export { default as ${element.name} } from './${element.name}';`)
+    .join('\n');
+
+  writeFile(path.resolve(__dirname, `${PATH_FOLDER_ICONS}/index.tsx`), `${entryText}`.trim());
+
+  console.log(`\n🎉 Generate Successfully!\n`);
+};
+
+generateIcons();
